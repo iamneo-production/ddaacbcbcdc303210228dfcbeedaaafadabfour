c11f21bc0d3dd5a5e82f42f2ffc73eba28378dce
Iamneo <techadmin@iamneo.ai>
2023-07-18 10:25:43 +0000

    Backup Commit

--- main.java
+++ main.java
@@ -1,5 +1,5 @@
 class Main {
-    public static void main() {
+    public static void main(String[] args) {
         System.out.println("Hello");
         for (int i = 0; i < 100; i++) {
             System.out.println("hello");
