e36dd228cd18ddef96c79db3a2d4ac1824d5ac36
Iamneo <techadmin@iamneo.ai>
2023-07-18 10:36:51 +0000

    Backup Commit

--- app/src/main/java/ai/iamneo/app/app/AppApplication.java
+++ app/src/main/java/ai/iamneo/app/app/AppApplication.java
@@ -8,6 +8,24 @@ public class AppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppApplication.class, args);
+		System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
 		for (int i = 0; i < 100; i++) {
 			System.out.println("Hello World");
 		}
